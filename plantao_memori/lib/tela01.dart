eef3ac7c38ad4ee0972a107b83de102e03b9f643
Tarcizio Dantas <tarciziomd@gmail.com>
2021-12-12 17:40:32 -0300

    criado arquivos dart

---
+++ plantao_memori/lib/tela01.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class tela01 extends StatefulWidget {
+  const tela01({ Key? key }) : super(key: key);
+
+  @override
+  _tela01State createState() => _tela01State();
+}
+
+class _tela01State extends State<tela01> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Controle de Plantões"),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
